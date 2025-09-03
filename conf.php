42bd0746aa53e58c425429bdd6a09d8151fa02b0
mwangi-stacy <njoki.mwangi@strathmore.edu>
2025-09-03 08:05:17 +0300

    Committing all changes : TOL project

---
+++ conf.php
@@ -0,0 +1,18 @@
+<?php
+// Site timezone
+$conf['site_timezone'] = 'Africa/Nairobi';
+
+// Site information
+$conf['site_name'] = 'ICS B Academy';
+$conf['site_url'] = 'http://localhost:8000/tol';
+$conf['admin_email'] = 'admin@icsbacademy.com';
+
+// Site language
+$conf['site_lang'] = 'en';
+
+// Database configuration
+$conf['db_type'] = 'pdo';
+$conf['db_host'] = 'localhost';
+$conf['db_user'] = 'root';
+$conf['db_pass'] = 'STANJO.O4#';
+$conf['db_name'] = 'tol';
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    require 'ClassAutoLoad.php';
+
+    // Using the class methods
+    print $layout->header($conf);
+    print $hello->today();
+    $form->signup();
+    print $layout->footer($conf);
+
+?>
